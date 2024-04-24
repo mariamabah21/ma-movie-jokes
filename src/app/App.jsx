f714ca86d20e6fe90464411ea5de5921783b7818
mariamabah21 <136363278+mariamabah21@users.noreply.github.com>
2024-04-24 23:47:18 +0200

    changes24.04

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,18 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import MovieDetails from "src/features/movies/MovieDetails";
 import MoviesList from "src/features/movies/MoviesList";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/">
-        <Route index element={<MoviesList />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />} />
-      </Route>
-    </Routes>
+    <ChakraProvider>
+      <Routes>
+        <Route path="/">
+          <Route index element={<MoviesList />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+        </Route>
+      </Routes>
+    </ChakraProvider>
   );
 }
 
--- src/features/movies/MoviesList.jsx
+++ src/features/movies/MoviesList.jsx
@@ -1,10 +1,19 @@
 import { Link } from "react-router-dom";
+import { data } from "./data";
+import MovieCard from "./MovieCard";
+import { SimpleGrid } from "@chakra-ui/react";
 
 function MoviesList() {
   return (
-    <div>
-      MoviesList <Link to={"/movies/dsds"}> Go to movie</Link>
-    </div>
+    <SimpleGrid spacing={4} columns={4}>
+      {data.results.map((movie) => (
+        <Link key={movie.id} to={`/movies/${movie.id}`}>
+          {" "}
+          {movie.title}
+          <MovieCard />
+        </Link>
+      ))}
+    </SimpleGrid>
   );
 }
 
