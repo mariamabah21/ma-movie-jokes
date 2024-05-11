82a042755260608890befb2746909be725157797
mariamabah21 <136363278+mariamabah21@users.noreply.github.com>
2024-05-11 20:22:39 +0200

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "src/app/App.jsx";
+import { store } from "./app/store";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
